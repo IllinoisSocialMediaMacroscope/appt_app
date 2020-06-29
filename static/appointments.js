90d3e265904053ef3325c0cd6456377e5817af14
Chen Wang <cwang138@illinois.edu>
2020-06-29 16:13:45 -0500

    program the filter dropdowns

--- static/appointments.js
+++ static/appointments.js
@@ -14,17 +14,40 @@ $(document).ready(function(){
     });
 });
 
-$("#location-select").on("change", function(){
+$("#location-select").on("change", function () {
     $("#available-appointments").find('tbody').empty();
+    filterOnChange();
+});
+
+$("#date-select").on("change", function () {
+    $("#available-appointments").find('tbody').empty();
+    filterOnChange();
+});
+
+$("#timeslot-select").on("change", function () {
+    $("#available-appointments").find('tbody').empty();
+    filterOnChange();
+});
+
+function filterOnChange() {
     var location = $("#location-select option:selected").val();
+    if ($("#date-select").val() !== "" && $("#date-select").val() !== undefined) {
+        var date = $("#date-select").val();
+    } else {
+        var date = "";
+    }
+
+    var time = $("#timeslot-select option:selected").val();
     $.ajax({
         url: "list",
         type: "GET",
         data: {
-            "location":location
+            "location": location,
+            "date": date,
+            "time": time,
         },
         success: function (data) {
-            if ("available_slots" in data){
+            if ("available_slots" in data) {
                 updateAppointmentTable(data.available_slots)
             }
         },
@@ -32,17 +55,17 @@ $("#location-select").on("change", function(){
             console.log(jqXHR.responseText);
         }
     });
-});
+}
 
-function updateAppointmentTable(available_slots){
-    $.each(available_slots, function(i, item){
+function updateAppointmentTable(available_slots) {
+    $.each(available_slots, function (i, item) {
         $("#available-appointments").find('tbody').append(
             "<tr>" +
-                "<td>" +
-                    "<input type=\"checkbox\" id=\"" + item.id + "\" name=\"submit\" value=\"" + item.id + "\">" +
-                "</td>" +
-                "<td>" + item.location + "</td>" +
-                "<td>" + item.date + "</td>" +
+            "<td>" +
+            "<input type=\"checkbox\" id=\"" + item.id + "\" name=\"submit\" value=\"" + item.id + "\">" +
+            "</td>" +
+            "<td>" + item.location + "</td>" +
+            "<td>" + item.date + "</td>" +
                 "<td>" + item.time + "</td>"
             + "</tr>");
     })
