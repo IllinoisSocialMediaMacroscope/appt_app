5eb610724bda9b426f584670ab5487c474bc9bb3
Chen Wang <cwang138@illinois.edu>
2020-07-06 12:00:08 -0500

    remove sensitive info

--- scripts/command.sh
+++ scripts/command.sh
@@ -1,3 +1,3 @@
 export FLASK_APP=app.py
 export FLASK_ENV=development
-python3 -m flask run --host=0.0.0.0 # running on public ip; for my machine it's ***REMOVED***
+python3 -m flask run --host=0.0.0.0 # running on public ip; for my machine it's 192.168.0.11
